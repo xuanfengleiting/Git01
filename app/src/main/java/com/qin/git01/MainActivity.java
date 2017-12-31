9834f672673cef139d34effe606e109353cf0308
xuanfeng <2500280714@qq.com>
2017-12-31 12:00:59 +0800

    添加一个log.

--- app/src/main/java/com/qin/git01/MainActivity.java
+++ app/src/main/java/com/qin/git01/MainActivity.java
@@ -2,12 +2,19 @@ package com.qin.git01;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static final String TAG="QBH";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.i(TAG, "onCreate: "+"HAHAHA");
+        Toast.makeText(this, "init ok", Toast.LENGTH_SHORT).show();
+
+
     }
 }
